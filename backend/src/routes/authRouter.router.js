f4a2f5ed7b9531f3468d67d771d26084f61f2d5e
Subhash Kumar <130248657+subhashkrgupta@users.noreply.github.com>
2026-03-20 15:38:31 +0530

    git isauthenticated middelware in the getme route

--- backend/src/routes/authRouter.router.js
+++ backend/src/routes/authRouter.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { loginController, logoutController, refreshToken, registerController } from "../controllers/auth.controller.js";
+import { isAuthenticated } from "../middleware/auth.middleware.js";
 
 const authRouter = Router();
 
@@ -7,6 +8,6 @@ authRouter.post('/register', registerController);
 authRouter.post('/login',loginController)
 authRouter.post('/refreshToken',refreshToken)
 authRouter.post('/logout' , logoutController)
-authRouter.get('/get-me',getMe)
+authRouter.get('/get-me',isAuthenticated,getMe)
 
 export default authRouter;
