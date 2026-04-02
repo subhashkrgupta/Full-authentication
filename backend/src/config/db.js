731a7a0de3ccc6e61b251890328e7b5545abd872
Subhash Kumar <130248657+subhashkrgupta@users.noreply.github.com>
2026-04-02 21:33:01 +0530

    change db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async ()=>{
     try {
+        if(!process.env.DATABASE_URL){
+            console.error('DATABASE_URL is not defined');
+            process.exit(1);
+        }
         await mongoose.connect(process.env.DATABASE_URL);
         console.log('MongoDB connected successfully');
     } catch (error) {
